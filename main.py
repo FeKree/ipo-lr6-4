3a29f2f88859cf2e73b4a108739fd92ee4126375
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-11-15 15:23:06 +0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 search =[] #Список создаем для вывода строк которые будем сортировать
 count = 0 #Переменная счетчик
 search_str = input("Напишите строку которая будет использоваться для поиска: ") #Просим ввести строку для поиска
-with open("D:\\profiles\\ShatukA_82\\Desktop\\Новая папка\\text.txt", 'r', encoding='utf-8') as file: #Читаем файл text.txt через 'r' 
+with open("text.txt", 'r', encoding='utf-8') as file: #Читаем файл text.txt через 'r' 
     for i in file: #Цикл для поиска и добавления строк в список
         if search_str in i: #Доп проверка подстроки
             search.append(i) #Добавляем в список строки
